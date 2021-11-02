a91f2554cac678b23bdeedbae89178de0a376b47
Jonas Hoebenreich <jonas.hoebenreich@tum.de>
2021-11-02 19:34:57 +0100

    stable tag 1.0.4

--- readme.txt
+++ readme.txt
@@ -3,7 +3,7 @@ Contributors: flinnn
 Tags: donation, donations, crowdfunding, fundraising, woocommerce
 Requires at least: 5.7
 Tested up to: 5.8.1
-Stable tag: 1.0.3
+Stable tag: 1.0.4
 Requires PHP: 7.1
 License: GPLv2 or later
 License URI: https://www.gnu.org/licenses/gpl-2.0.html
--- wc-donation-platform.php
+++ wc-donation-platform.php
@@ -4,7 +4,7 @@
  * Plugin URI: https://wcdp.jonh.eu/
  * Description: Donation Platform for WooCommerce unlocks the power of WooCommerce for your online fundraising & crowdfunding.
  * Author: Jonas Höbenreich
- * Version: 1.0.3
+ * Version: 1.0.4
  * Author URI: https://www.jonh.eu/
  * Plugin URI:  https://wcdp.jonh.eu/
  * License: GNU General Public License v3.0
