e7b7fd03913880f1c55aa39b342855b521c64470
jonas-hoebenreich <jonas.hoebenreich@tum.de>
2023-03-08 17:44:47 +0100

    fix incompatibility with Subscriptions for WooCommerce add rename_menu_item function Update class-wcdp-subscriptions-for-woocommerce.php

--- includes/integrations/subscriptions-for-woocommerce/class-wcdp-subscriptions-for-woocommerce.php
+++ includes/integrations/subscriptions-for-woocommerce/class-wcdp-subscriptions-for-woocommerce.php
@@ -1,5 +1,5 @@
 <?php
-if(!defined('ABSPATH')) exit;
+if (!defined('ABSPATH')) exit;
 
 /**
  * This class integrates Subscriptions for WooCommerce (Lite) with Donation Platform for WooCommerce
@@ -7,81 +7,102 @@ if(!defined('ABSPATH')) exit;
  */
 class WCDP_Subscriptions_For_WooCommerce
 {
-	/**
-	 * Bootstraps the class and hooks required actions & filters
-	 */
-	public static function init()
-	{
-		//set recurring total for regular donations
-		add_filter('wps_sfw_cart_data_for_susbcription', 'WCDP_Subscriptions_For_WooCommerce::price_subscription', 10, 2);
+    /**
+     * Bootstraps the class and hooks required actions & filters
+     */
+    public static function init()
+    {
+        //set recurring total for regular donations
+        add_filter('wps_sfw_cart_data_for_susbcription', 'WCDP_Subscriptions_For_WooCommerce::price_subscription', 10, 2);
 
-		//Filter specific Subscriptions for WooCommerce to WCDP templates
-		add_filter( 'wc_get_template', 'WCDP_Subscriptions_For_WooCommerce::modify_template', 10, 5 );
+        //Filter specific Subscriptions for WooCommerce to WCDP templates
+        add_filter('wc_get_template', 'WCDP_Subscriptions_For_WooCommerce::modify_template', 10, 5);
 
-		//Rename Subscriptions Tab on My Account page
-		add_filter( 'woocommerce_account_menu_items', 'WCDP_Subscriptions_For_WooCommerce::rename_menu_item', 11, 1 );
+        //Rename Subscriptions Tab on My Account page
+        add_filter('woocommerce_account_menu_items', 'WCDP_Subscriptions_For_WooCommerce::rename_menu_item', 11, 1);
 
-        add_filter( 'wps_sfw_check_pro_plugin', '__return_true' );
-	}
+        add_filter('wps_sfw_check_pro_plugin', '__return_true');
 
-	/**
-	 * Update donation amount of recurring donation
-	 * @param $mwb_recurring_data
-	 * @param $cart_item
-	 * @return mixed
-	 */
-	public static function price_subscription($mwb_recurring_data, $cart_item) {
-		$min_donation_amount = get_option('wcdp_min_amount', 3);
-		$max_donation_amount = get_option('wcdp_max_amount', 50000);
+        add_filter('wps_rbpfw_cart_price', 'WCDP_Subscriptions_For_WooCommerce::cart_price', 10, 2);
 
-		if( isset( $cart_item["wcdp_donation_amount"] ) &&
-			$cart_item["wcdp_donation_amount"] >= $min_donation_amount &&
-			$cart_item["wcdp_donation_amount"] <= $max_donation_amount
-		) {
-			$mwb_recurring_data['wps_recurring_total'] = $cart_item["wcdp_donation_amount"];
-		}
-		return $mwb_recurring_data;
-	}
+    }
 
-	/**
-	 * overwrite specific Subscriptions for WooCommerce templates to WCDP templates
-	 *
-	 * @param string $template
-	 * @param string $template_name
-	 * @param string $args
-	 * @param string $template_path
-	 * @param string $default_path
-	 * @return string
-	 */
-	public static function modify_template(string $template, string $template_name, $args, string $template_path, string $default_path ): string
-	{
-		//Only apply for Subscriptions for WooCommerce Templates
-		if (! strpos($default_path, 'subscriptions-for-woocommerce')) {
-			return $template;
-		}
-		//Return if the template has been overwritten in yourtheme/woocommerce/XXX
-		//Checks if it's woocommerce/ or templates/ as before $template_name
-		if ($template[strlen($template) - strlen($template_name) - 2] === 'e') {
-			return $template;
-		}
-		$path = WCDP_DIR . 'includes/integrations/subscriptions-for-woocommerce/templates/';
+    /**
+     * Update donation amount of recurring donation
+     * @param $mwb_recurring_data
+     * @param $cart_item
+     * @return mixed
+     */
+    public static function price_subscription($mwb_recurring_data, $cart_item)
+    {
+        if (isset($cart_item["wcdp_donation_amount"]) &&
+            isset($cart_item["product_id"]) &&
+            WCDP_Form::check_donation_amount($cart_item["wcdp_donation_amount"], $cart_item["product_id"])
+        ) {
+            $mwb_recurring_data['wps_recurring_total'] = $cart_item["wcdp_donation_amount"];
+        }
+        return $mwb_recurring_data;
+    }
 
-		if ($template_name == 'myaccount/mwb-show-subscription-details.php') {
+    /**
+     * overwrite specific Subscriptions for WooCommerce templates to WCDP templates
+     *
+     * @param string $template
+     * @param string $template_name
+     * @param $args
+     * @param string $template_path
+     * @param string $default_path
+     * @return string
+     */
+    public static function modify_template(string $template, string $template_name, $args, string $template_path, string $default_path): string
+    {
+        //Only apply for Subscriptions for WooCommerce Templates
+        if (!strpos($default_path, 'subscriptions-for-woocommerce')) {
+            return $template;
+        }
+        //Return if the template has been overwritten in yourtheme/woocommerce/XXX
+        //Checks if it's woocommerce/ or templates/ as before $template_name
+        if ($template[strlen($template) - strlen($template_name) - 2] === 'e') {
+            return $template;
+        }
+        $path = WCDP_DIR . 'includes/integrations/subscriptions-for-woocommerce/templates/';
+
+        if ($template_name == 'myaccount/mwb-show-subscription-details.php') {
             $template = $path . $template_name;
-		}
-		return apply_filters( 'wcdp_get_template', $template, $template_name, $args, $template_path, $default_path );
-	}
+        }
+        return apply_filters('wcdp_get_template', $template, $template_name, $args, $template_path, $default_path);
+    }
 
-	/**
-	 * Rename Menu item on Account page
-	 *
-	 * @param $menu_items
-	 * @return mixed
-	 */
-	public static function rename_menu_item($menu_items) {
-		if (array_key_exists('mwb_subscriptions', $menu_items)) {
-			$menu_items['mwb_subscriptions'] = __( 'Recurring Donations', 'wc-donation-platform' );
-		}
-		return $menu_items;
-	}
+    /**
+     * Rename Menu item on Account page
+     *
+     * @param $menu_items
+     * @return mixed
+     */
+    public static function rename_menu_item($menu_items)
+    {
+        if (array_key_exists('mwb_subscriptions', $menu_items)) {
+            $menu_items['mwb_subscriptions'] = __('Recurring Donations', 'wc-donation-platform');
+        }
+        return $menu_items;
+    }
+
+    /**
+     * Update subscription price in checkout order summary
+     * @param $product_price
+     * @param $cart_item
+     * @return mixed|string
+     * @since v1.2.10
+     */
+    public static function cart_price($product_price, $cart_item)
+    {
+        if (!isset($cart_item["wcdp_donation_amount"]) ||
+            !isset($cart_item["product_id"]) ||
+            !WCDP_Form::check_donation_amount($cart_item["wcdp_donation_amount"], $cart_item["product_id"])) {
+            return $product_price;
+        }
+
+        return wc_price($cart_item["wcdp_donation_amount"]);
+    }
+
 }
