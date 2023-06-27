99819b852b17139586d56f08aee51470102fb2cf
jonas-hoebenreich <jonas.hoebenreich@tum.de>
2023-06-28 00:42:08 +0200

    improve leaderboard Update class-wcdp-fee-recovery.php and class-wcdp_leaderboard.php

--- includes/class-wcdp-fee-recovery.php
+++ includes/class-wcdp-fee-recovery.php
@@ -1,41 +1,44 @@
 <?php
 
-if(!defined('ABSPATH')) exit;
+if (!defined('ABSPATH')) exit;
 
 /**
  * Class WCDP_Fee_Recovery
  */
 class WCDP_Fee_Recovery
 {
-	public function __construct() {
-		$fee_recovery_enabled = get_option('wcdp_fee_recovery', 'no');
-		if ($fee_recovery_enabled == 'yes') {
-			//Add Checkbox to WC checkout
-			add_action( 'wcdp_fee_recovery', array($this, 'add_fee_recovery_checkbox') );
+    public function __construct()
+    {
+        $fee_recovery_enabled = get_option('wcdp_fee_recovery', 'no');
+        if ($fee_recovery_enabled == 'yes') {
+            //Add Checkbox to WC checkout
+            add_action('wcdp_fee_recovery', array($this, 'add_fee_recovery_checkbox'));
 
-			//Add transaction fee to WC cart
-			add_action( 'woocommerce_cart_calculate_fees', array($this, 'add_transaction_fee_cart'), 10, 1 );
-		}
-		//Add Fee recovery options table to general setting
-		add_action( 'woocommerce_admin_field_wcdp_fee_recovery', array($this, 'settings_fee_recovery') );
-	}
+            //Add transaction fee to WC cart
+            add_action('woocommerce_cart_calculate_fees', array($this, 'add_transaction_fee_cart'), 10, 1);
+        }
+        //Add Fee recovery options table to general setting
+        add_action('woocommerce_admin_field_wcdp_fee_recovery', array($this, 'settings_fee_recovery'));
+    }
 
-	/**
-	 * Add a checkbox to the WooCommerce checkout form
-	 * @return void
-	 */
-	function add_fee_recovery_checkbox() {
-		if (WCDP_Form::cart_contains_donation()) {
-			?>
-				<tr class="wcdp-fee-recovery-row">
-					<th colspan="2">
-						<label class="wcdp-fee-recovery" for="wcdp_fee_recovery">
-							<input type="checkbox" id="wcdp_fee_recovery" class="wcdp-fee-recovery__input" name="wcdp_fee_recovery" value="wcdp_fee_recovery" style="display:none;"
-									<?php if(isset($_POST['post_data']) && strpos($_POST['post_data'], 'wcdp_fee_recovery=wcdp_fee_recovery')): ?>
-										checked="checked"
-									<?php endif; ?>
-							>
-							<span class="wcdp-fee-recovery__body">
+    /**
+     * Add a checkbox to the WooCommerce checkout form
+     * @return void
+     */
+    function add_fee_recovery_checkbox()
+    {
+        if (WCDP_Form::cart_contains_donation()) {
+            ?>
+            <tr class="wcdp-fee-recovery-row">
+                <th colspan="2">
+                    <label class="wcdp-fee-recovery" for="wcdp_fee_recovery">
+                        <input type="checkbox" id="wcdp_fee_recovery" class="wcdp-fee-recovery__input"
+                               name="wcdp_fee_recovery" value="wcdp_fee_recovery" style="display:none;"
+                            <?php if (isset($_POST['post_data']) && strpos($_POST['post_data'], 'wcdp_fee_recovery=wcdp_fee_recovery')): ?>
+                                checked="checked"
+                            <?php endif; ?>
+                        >
+                        <span class="wcdp-fee-recovery__body">
 								<span></span>
 								<span class="wcdp-fee-recovery__body-header">
 									<?php esc_html_e('Yes, I want to cover the transaction fee.', 'wc-donation-platform'); ?>
@@ -46,153 +49,155 @@ class WCDP_Fee_Recovery
 									</svg>
 								</span>
 							</span>
-						</label>
-					</th>
-				</tr>
-			<?php
-		}
-	}
+                    </label>
+                </th>
+            </tr>
+            <?php
+        }
+    }
 
     /**
      * Add the transaction fee to cart if wcdp_fee_recovery checked
      * @param $cart WC_Cart
      * @return void
      */
-	function add_transaction_fee_cart(WC_Cart $cart ) {
-		if (is_admin() && !defined('DOING_AJAX') || is_null(WC()->cart)) {
-			return;
-		}
+    function add_transaction_fee_cart(WC_Cart $cart)
+    {
+        if (is_admin() && !defined('DOING_AJAX') || is_null(WC()->cart)) {
+            return;
+        }
         //determine currently selected payment method
-		if (isset($_POST['post_data']) && strpos($_POST['post_data'], 'wcdp_fee_recovery=wcdp_fee_recovery')) {
-			$post_data = $_POST['post_data'];
-			$pos = strpos($post_data, 'payment_method=');
-			if ($pos) {
-				$payment_method = sanitize_key(strtok(substr($post_data, $pos+15), '&'));
-			} else {
-				$payment_method = '';
-			}
-		} else if (isset($_POST['wcdp_fee_recovery']) && isset($_POST['payment_method'])) {
-			$payment_method = sanitize_key($_POST['payment_method']);
-		} else {
-			return;
-		}
+        if (isset($_POST['post_data']) && strpos($_POST['post_data'], 'wcdp_fee_recovery=wcdp_fee_recovery')) {
+            $post_data = $_POST['post_data'];
+            $pos = strpos($post_data, 'payment_method=');
+            if ($pos) {
+                $payment_method = sanitize_key(strtok(substr($post_data, $pos + 15), '&'));
+            } else {
+                $payment_method = '';
+            }
+        } else if (isset($_POST['wcdp_fee_recovery']) && isset($_POST['payment_method'])) {
+            $payment_method = sanitize_key($_POST['payment_method']);
+        } else {
+            return;
+        }
         //Get fees for all payment options
-		$wcdp_fee_recovery_values = json_decode(get_option( 'wcdp_fee_recovery_values', '{}' ), true);
+        $wcdp_fee_recovery_values = json_decode(get_option('wcdp_fee_recovery_values', '{}'), true);
 
         //Get fees for selected payment method
-		if (isset($wcdp_fee_recovery_values[$payment_method])) {
-			$value_fixed = 0;
-			$value_variable = 0;
-			if (isset($wcdp_fee_recovery_values[$payment_method]['fixed']) &&
-				$wcdp_fee_recovery_values[$payment_method]['fixed'] > 0) {
-				$value_fixed = (float) $wcdp_fee_recovery_values[$payment_method]['fixed'];
-			}
-			if (isset($wcdp_fee_recovery_values[$payment_method]['variable']) &&
-				$wcdp_fee_recovery_values[$payment_method]['variable'] > 0 &&
-				$wcdp_fee_recovery_values[$payment_method]['variable'] <= 100
-			) {
-				$value_variable = (float) $wcdp_fee_recovery_values[$payment_method]['variable'];
-			}
+        if (isset($wcdp_fee_recovery_values[$payment_method])) {
+            $value_fixed = 0;
+            $value_variable = 0;
+            if (isset($wcdp_fee_recovery_values[$payment_method]['fixed']) &&
+                $wcdp_fee_recovery_values[$payment_method]['fixed'] > 0) {
+                $value_fixed = (float)$wcdp_fee_recovery_values[$payment_method]['fixed'];
+            }
+            if (isset($wcdp_fee_recovery_values[$payment_method]['variable']) &&
+                $wcdp_fee_recovery_values[$payment_method]['variable'] > 0 &&
+                $wcdp_fee_recovery_values[$payment_method]['variable'] <= 100
+            ) {
+                $value_variable = (float)$wcdp_fee_recovery_values[$payment_method]['variable'];
+            }
 
-			$amount = $cart->get_cart_contents_total();
-			$fee = $value_fixed + $value_variable/100 * $amount;
+            $amount = $cart->get_cart_contents_total();
+            $fee = $value_fixed + $value_variable / 100 * $amount;
             $cart->add_fee(__('Transaction costs', 'wc-donation-platform'), $fee);
-		}
-	}
+        }
+    }
 
-	/**
-	 * Add Fee recovery options table to general setting
-	 * @param $value
-	 * @return void
-	 */
-	function settings_fee_recovery($value) {
-		?>
-		<script>
-			(function($) {
-				$(window).bind("load", function() {
-					show_hide_fee_recovery();
-				});
+    /**
+     * Add Fee recovery options table to general setting
+     * @param $value
+     * @return void
+     */
+    function settings_fee_recovery($value)
+    {
+        ?>
+        <script>
+            (function ($) {
+                $(window).bind("load", function () {
+                    show_hide_fee_recovery();
+                });
 
-				$('#wcdp_fee_recovery').on('change', function(){
-					show_hide_fee_recovery();
-				});
+                $('#wcdp_fee_recovery').on('change', function () {
+                    show_hide_fee_recovery();
+                });
 
-				function show_hide_fee_recovery() {
-					if ( $('#wcdp_fee_recovery').prop('checked') ) {
-						$( '.show_if_fee_recovery' ).show();
-					} else {
-						$( '.show_if_fee_recovery' ).hide();
-					}
-				}
-			})(jQuery);
-		</script>
-		<tr class="show_if_fee_recovery">
-			<th scope="row" class="titledesc"></th>
-			<td class="forminp forminp-checkbox">
-				<fieldset>
-					<?php
-					$available_payment_methods = WC()->payment_gateways->get_available_payment_gateways();
-					if (empty($available_payment_methods)):
-						esc_html_e('No active payment methods found.', 'wc-donation-platform');
-					else:
-						esc_html_e('Enter the fixed transaction costs in the left field and the percentage transaction costs in the right field.', 'wc-donation-platform');
-					?>
-						<table>
-							<?php
-							$wcdp_fee_recovery_values = json_decode(get_option( 'wcdp_fee_recovery_values', '{}' ), true);
-							foreach( $available_payment_methods as $method ) {
-								$value_fixed = 0;
-								$value_variable = 0;
-								if (isset($wcdp_fee_recovery_values[$method->id])) {
-									if (isset($wcdp_fee_recovery_values[$method->id]['fixed']) &&
-											$wcdp_fee_recovery_values[$method->id]['fixed'] >= 0) {
-										$value_fixed = $wcdp_fee_recovery_values[$method->id]['fixed'];
-									}
-									if (isset($wcdp_fee_recovery_values[$method->id]['variable']) &&
-											$wcdp_fee_recovery_values[$method->id]['variable'] >= 0 &&
-											$wcdp_fee_recovery_values[$method->id]['variable'] <= 100
-									) {
-										$value_variable = $wcdp_fee_recovery_values[$method->id]['variable'];
-									}
-								}
-								?>
-								<tr>
-									<td>
-										<label><?php echo esc_html($method->get_title()); ?></label>
-									</td>
-									<td>
-										<?php echo get_woocommerce_currency_symbol(); ?>
-										<input
-												name="<?php echo esc_attr( 'wcdp_fixed_' . $method->id  ); ?>"
-												id="<?php echo esc_attr( 'wcdp_fixed_' . $method->id ); ?>"
-												type="number"
-												style="width: 100px;"
-												value="<?php echo esc_attr($value_fixed); ?>"
-												placeholder="<?php esc_html_e( 'Fixed', 'wc-donation-platform' ); ?>"
-												min="0"
-												step="<?php echo esc_attr($value['step']); ?>"
-										/> <?php esc_html_e('(fixed)', 'wc-donation-platform'); ?>&nbsp;&nbsp;&nbsp;
-										<input
-												name="<?php echo esc_attr( 'wcdp_variable_'. $method->id ); ?>"
-												id="<?php echo esc_attr( 'wcdp_variable_' . $method->id ); ?>"
-												type="number"
-												style="width: 100px;"
-												value="<?php echo esc_attr($value_variable); ?>"
-												placeholder="<?php esc_html_e( 'Variable', 'wc-donation-platform' ); ?>"
-												min="0"
-												max="100"
-												step="any"
-										/>% <?php esc_html_e('(variable)', 'wc-donation-platform'); ?>
-									</td>
-								</tr>
-								<?php
-							}
-							?>
-						</table>
-					<?php endif; ?>
-				</fieldset>
-			</td>
-		</tr>
-		<?php
-	}
+                function show_hide_fee_recovery() {
+                    if ($('#wcdp_fee_recovery').prop('checked')) {
+                        $('.show_if_fee_recovery').show();
+                    } else {
+                        $('.show_if_fee_recovery').hide();
+                    }
+                }
+            })(jQuery);
+        </script>
+        <tr class="show_if_fee_recovery">
+            <th scope="row" class="titledesc"></th>
+            <td class="forminp forminp-checkbox">
+                <fieldset>
+                    <?php
+                    $available_payment_methods = WC()->payment_gateways->get_available_payment_gateways();
+                    if (empty($available_payment_methods)):
+                        esc_html_e('No active payment methods found.', 'wc-donation-platform');
+                    else:
+                        esc_html_e('Enter the fixed transaction costs in the left field and the percentage transaction costs in the right field.', 'wc-donation-platform');
+                        ?>
+                        <table>
+                            <?php
+                            $wcdp_fee_recovery_values = json_decode(get_option('wcdp_fee_recovery_values', '{}'), true);
+                            foreach ($available_payment_methods as $method) {
+                                $value_fixed = 0;
+                                $value_variable = 0;
+                                if (isset($wcdp_fee_recovery_values[$method->id])) {
+                                    if (isset($wcdp_fee_recovery_values[$method->id]['fixed']) &&
+                                        $wcdp_fee_recovery_values[$method->id]['fixed'] >= 0) {
+                                        $value_fixed = $wcdp_fee_recovery_values[$method->id]['fixed'];
+                                    }
+                                    if (isset($wcdp_fee_recovery_values[$method->id]['variable']) &&
+                                        $wcdp_fee_recovery_values[$method->id]['variable'] >= 0 &&
+                                        $wcdp_fee_recovery_values[$method->id]['variable'] <= 100
+                                    ) {
+                                        $value_variable = $wcdp_fee_recovery_values[$method->id]['variable'];
+                                    }
+                                }
+                                ?>
+                                <tr>
+                                    <td>
+                                        <label><?php echo esc_html($method->get_title()); ?></label>
+                                    </td>
+                                    <td>
+                                        <?php echo get_woocommerce_currency_symbol(); ?>
+                                        <input
+                                                name="<?php echo esc_attr('wcdp_fixed_' . $method->id); ?>"
+                                                id="<?php echo esc_attr('wcdp_fixed_' . $method->id); ?>"
+                                                type="number"
+                                                style="width: 100px;"
+                                                value="<?php echo esc_attr($value_fixed); ?>"
+                                                placeholder="<?php esc_html_e('Fixed', 'wc-donation-platform'); ?>"
+                                                min="0"
+                                                step="<?php echo esc_attr($value['step']); ?>"
+                                        /> <?php esc_html_e('(fixed)', 'wc-donation-platform'); ?>&nbsp;&nbsp;&nbsp;
+                                        <input
+                                                name="<?php echo esc_attr('wcdp_variable_' . $method->id); ?>"
+                                                id="<?php echo esc_attr('wcdp_variable_' . $method->id); ?>"
+                                                type="number"
+                                                style="width: 100px;"
+                                                value="<?php echo esc_attr($value_variable); ?>"
+                                                placeholder="<?php esc_html_e('Variable', 'wc-donation-platform'); ?>"
+                                                min="0"
+                                                max="100"
+                                                step="any"
+                                        />% <?php esc_html_e('(variable)', 'wc-donation-platform'); ?>
+                                    </td>
+                                </tr>
+                                <?php
+                            }
+                            ?>
+                        </table>
+                    <?php endif; ?>
+                </fieldset>
+            </td>
+        </tr>
+        <?php
+    }
 }
--- includes/class-wcdp_leaderboard.php
+++ includes/class-wcdp_leaderboard.php
@@ -5,19 +5,145 @@
  * * @since 1.3.0
  */
 
-if(!defined('ABSPATH')) exit;
+if (!defined('ABSPATH')) exit;
 
 class WCDP_Leaderboard
 {
     /**
      * Bootstraps the class and hooks required actions & filters.
      */
-    public function __construct() {
+    public function __construct()
+    {
         //Leaderboard shortcode
-        add_shortcode( 'wcdp_leaderboard', array($this, 'wcdp_leaderboard'));
+        add_shortcode('wcdp_leaderboard', array($this, 'wcdp_leaderboard'));
 
         //Delete cache on order change
-        add_action('woocommerce_order_status_transition_completed', 'delete_old_latest_orders_cache', 10, 4);
+        add_action('woocommerce_order_status_changed', array($this, 'delete_old_latest_orders_cache'), 10, 4);
+    }
+
+    /**
+     * Rteunrs an array with all WooCommerce orders
+     * @return array
+     */
+    private function get_orders_db(): array
+    {
+        $args = array(
+            'limit' => 1000,
+            'status' => 'completed',
+            'orderby' => 'date',
+            'order'   => 'DESC',
+        );
+        $all_orders = wc_get_orders($args);
+
+        $orders_clean = array();
+        foreach ($all_orders as $order) {
+            $product_ids = array();
+            foreach ($order->get_items() as $item) {
+                $product_ids[] = $item->get_product_id();
+            }
+
+            $orders_clean[] = array(
+                'date' => $order->get_date_created()->getTimestamp(),
+                'first' => $order->get_billing_first_name(),
+                'last' =>  $order->get_billing_last_name(),
+                'city' => $order->get_billing_city(),
+                'country' => $order->get_billing_country(),
+                'zip' => $order->get_billing_postcode(),
+                'total' => $order->get_total(),
+                'cy' => $order->get_currency(),
+                'ids' => $product_ids,
+            );
+        }
+
+        // Sort the orders by date, newest first
+        usort($orders_clean, function ($a, $b) {
+            return strtotime($b['date']) - strtotime($a['date']);
+        });
+
+        return $orders_clean;
+    }
+
+    /**
+     * Return all the latest WooCommerce orders
+     * @return array|mixed
+     */
+    private function get_orders() : array {
+        $cache_key = 'wcdp_latest_orders';
+        $all_orders = json_decode(get_transient($cache_key), true);
+
+        if (empty($all_orders)) {
+            $all_orders = $this->get_orders_db();
+            set_transient($cache_key, json_encode($all_orders), apply_filters("wcdp_cache_expiration", 6 * HOUR_IN_SECONDS));
+        }
+        return $all_orders;
+    }
+
+    /**
+     * Return orders that included at least one of the specified ids
+     * @param $limit
+     * @param $ids
+     * @return array
+     */
+    private function wcdp_get_latest_orders($limit, $ids): array
+    {
+        $all_orders = $this->get_orders();
+        $filtered_orders = array_filter($all_orders, function ($order) use ($ids) {
+            return !empty(array_intersect($order['ids'], $ids));
+        });
+        if ($limit === -1) {
+            return $filtered_orders;
+        }
+        return array_slice($filtered_orders, 0, $limit);
+    }
+
+// Generate the HTML output for the orders
+    public function wcdp_generate_order_output($orders, $title, $subtitle): string
+    {
+        $title = sanitize_text_field($title);
+        $subtitle = sanitize_text_field($subtitle);
+
+        $output = '
+            <style>
+                .wcdp-leaderboard {
+                  list-style: none;
+                  padding: 0;
+                  margin: 0;
+                }
+                
+                .wcdp-leaderboard .wcdp-leaderboard-li {
+                  padding: 10px;
+                  margin-bottom: 10px;
+                }
+                
+                .wcdp-leaderboard .wcdp-leaderboard-title, .wcdp-leaderboard .woocommerce-Price-amount, .wcdp-leaderboard .wcdp-emphasized {
+                  font-weight: bold;
+                }
+            </style>
+            <ul class="wcdp-leaderboard">';
+        foreach ($orders as $order) {
+            $placeholders = array(
+                '{firstname}' => esc_html($order['first']),
+                '{firstname_initial}' => esc_html($this->get_initials($order['first'])),
+                '{lastname_initial}' => esc_html($this->get_initials($order['last'])),
+                '{amount}' => wc_price($order['total'], array('currency' => $order['cy'],)),
+                '{timediff}' => $this->get_human_time_diff($order['date']),
+                '{datetime}' => date_i18n(get_option('date_format') . ' ' . get_option('time_format'), $order['date']),
+                '{date}' => date_i18n(get_option('date_format'), $order['date']),
+                '{city}' => esc_html($order['city']),
+                '{country}' => WC()->countries->countries[esc_attr($order['country'])],
+                '{country_code}' => esc_html($order['country']),
+                '{postcode}' => esc_html($order['zip']),
+                '{currency}' => esc_html($order['cy']),
+            );
+
+            $output .= '<li class="wcdp-leaderboard-li">';
+            $output .= '<span class="wcdp-leaderboard-title">' . strtr($title, $placeholders) . '</span><br>';
+            $output .= '<span class="wcdp-leaderboard-subtitle">' . strtr($subtitle, $placeholders) . '</span><br>';
+            $output .= '</li>';
+        }
+        $output .= '</ul>';
+
+        return $output;
     }
 
     /**
@@ -27,80 +153,63 @@ class WCDP_Leaderboard
      */
     function wcdp_leaderboard($atts): string
     {
+        // Do not allow executing this Shortcode via AJAX
+        if (wp_doing_ajax()) return "";
+
         // Extract attributes
         $atts = shortcode_atts(array(
             'limit' => 100,
-            'ids' => ''
+            'ids' => '',
+            'title' => '{firstname} donated {amount}',
+            'subtitle' => '{timediff}',
         ), $atts, 'latest_orders');
 
         $limit = intval($atts['limit']);
-        $ids = sanitize_text_field($atts['ids']);
-        $cache_key = 'wcdp_latest_orders_' . $limit . '_' . $ids;
+        $ids = explode(',', $atts['ids']);;
 
-        // Try to get the orders from cache
-        $orders = get_transient($cache_key);
-        $expiration = apply_filters("wcdp_leaderboard_cache_time", 6 * HOUR_IN_SECONDS);
+        // Get the latest orders
+        $orders = $this->wcdp_get_latest_orders($limit, $ids);
 
-        // If cache is empty, fetch the latest orders
-        if (empty($orders)) {
-            $args = array(
-                'limit' => $limit,
-                'status' => 'completed',
-                'include' => explode(',', $ids),
-            );
-            $orders = wc_get_orders($args);
-
-            set_transient($cache_key, $orders, $expiration);
-        }
-
-        $output = '<ul>';
-        foreach ($orders as $order) {
-            $order_id = $order->get_id();
-            $order_date = $order->get_date_created()->date('Y-m-d');
-            $customer = $order->get_billing_first_name() . ' ' . $order->get_billing_last_name();
-            $city = $order->get_billing_city();
-            $country = $order->get_billing_country();
-            $amount = $order->get_total();
-
-            $output .= '<li>';
-            $output .= 'Order ID: ' . $order_id . '<br>';
-            $output .= 'Order Date: ' . $order_date . '<br>';
-            $output .= 'Customer Name: ' . $customer . '<br>';
-            $output .= 'Customer City: ' . $city . '<br>';
-            $output .= 'Customer Country: ' . $country . '<br>';
-            $output .= 'Amount: ' . $amount . '<br>';
-            $output .= '</li>';
-        }
-        $output .= '</ul>';
-
-        return $output;
+        // Generate the HTML output
+        return $this->wcdp_generate_order_output($orders, $atts['title'], $atts['subtitle']);
     }
 
     function delete_old_latest_orders_cache($order_id, $old_status, $new_status, $order): void
     {
-        if ($old_status !== 'completed' && $new_status !==  'completed') return;
+        if ($old_status !== 'completed' && $new_status !== 'completed') return;
 
-        $threshold = time() - 90;
+        $cache_key = 'wcdp_latest_orders';
+        $timeout = get_option('_transient_timeout_' . $cache_key);
 
-        // Get all transients matching the pattern "latest_orders_*"
-        $transients = get_option('alloptions');
-        $delete_keys = array();
-        foreach ($transients as $key => $value) {
-            if (str_starts_with($key, 'wcdp_latest_orders_')) {
-                // Extract the timestamp part of the cache key
-                $timestamp = substr($key, strrpos($key, '_') + 1);
-
-                // Delete the cache if the timestamp is older than the threshold
-                if ($timestamp < $threshold) {
-                    delete_transient($key);
-                    $delete_keys[] = $key;
-                }
-            }
+        if ($timeout && time() + apply_filters("wcdp_cache_expiration", 6 * HOUR_IN_SECONDS) - $timeout > 90) {
+            delete_transient($cache_key);
+            delete_transient($cache_key . '_timestamp');
         }
+    }
 
-        // Output the deleted cache keys (for debugging purposes)
-        if (!empty($delete_keys)) {
-            error_log('Deleted latest orders cache keys: ' . implode(', ', $delete_keys));
+    /**
+     * Get the initials of a name
+     * @param $name
+     * @return string
+     */
+    private function get_initials($name): string
+    {
+        $parts = explode(' ', $name);
+        $initials = '';
+        foreach ($parts as $part) {
+            $initials .= strtoupper(substr($part, 0, 1));
         }
+        return $initials;
+    }
+
+    /**
+     * Returns human time diff. Expects $timestamp to be in the past
+     *
+     * @param $timestamp int UNIX timestamp
+     * @return string
+     */
+    private function get_human_time_diff(int $timestamp ): string {
+        $human_diff = '<span class="wcdp-emphasized">' . human_time_diff( $timestamp ) . '</span>';
+        return sprintf( __( '%s ago', 'wc-donation-platform' ), $human_diff );
     }
 }
