c0082eefe78d3d7fffda35a09011cacf8a1940d2
jonas-hoebenreich <jonas.hoebenreich@tum.de>
2024-06-14 00:03:16 +0200

    update template Update thankyou.php

--- includes/wc-templates/checkout/thankyou.php
+++ includes/wc-templates/checkout/thankyou.php
@@ -9,9 +9,14 @@
  * maintain compatibility. We try to do this as little as possible, but it does
  * happen. When this occurs the version of the template file will be bumped and
  * the readme will list any important changes.
- *
- * @see https://docs.woocommerce.com/document/template-structure/
+ * 
  * forked from WooCommerce\Templates
+ *
+ * @see https://woocommerce.com/document/template-structure/
+ * @package WooCommerce\Templates
+ * @version 8.1.0
+ *
+ * @var WC_Order $order
  */
 
 defined('ABSPATH') || exit;
@@ -20,74 +25,80 @@ defined('ABSPATH') || exit;
 <div class="woocommerce-order">
 
     <?php
-    if ($order) :
+    if ($order):
 
         do_action('woocommerce_before_thankyou', $order->get_id());
         ?>
 
-        <?php if ($order->has_status('failed')) : ?>
+        <?php if ($order->has_status('failed')): ?>
 
-        <p class="woocommerce-notice woocommerce-notice--error woocommerce-thankyou-order-failed"><?php esc_html_e('Unfortunately your donation cannot be processed as the originating bank/merchant has declined your transaction. Please attempt your donation again.', 'wc-donation-platform'); ?></p>
+            <p class="woocommerce-notice woocommerce-notice--error woocommerce-thankyou-order-failed">
+                <?php esc_html_e('Unfortunately your donation cannot be processed as the originating bank/merchant has declined your transaction. Please attempt your donation again.', 'wc-donation-platform'); ?>
+            </p>
 
-        <p class="woocommerce-notice woocommerce-notice--error woocommerce-thankyou-order-failed-actions">
-            <a href="<?php echo esc_url($order->get_checkout_payment_url()); ?>"
-               class="button pay"><?php esc_html_e('Pay', 'woocommerce'); ?></a>
-            <?php if (is_user_logged_in()) : ?>
-                <a href="<?php echo esc_url(wc_get_page_permalink('myaccount')); ?>"
-                   class="button pay"><?php esc_html_e('My account', 'woocommerce'); ?></a>
-            <?php endif; ?>
-        </p>
 
-    <?php else : ?>
+            <p class="woocommerce-notice woocommerce-notice--error woocommerce-thankyou-order-failed-actions">
+                <a href="<?php echo esc_url($order->get_checkout_payment_url()); ?>"
+                    class="button pay"><?php esc_html_e('Pay', 'woocommerce'); ?></a>
+                <?php if (is_user_logged_in()): ?>
+                    <a href="<?php echo esc_url(wc_get_page_permalink('myaccount')); ?>"
+                        class="button pay"><?php esc_html_e('My account', 'woocommerce'); ?></a>
+                <?php endif; ?>
+            </p>
 
-        <h1 class="wcdp-heading wcdp-donation-received-heading"><?php echo apply_filters('wcdp_thankyou_donation_received_heading', esc_html__('Thank you for your donation', 'wc-donation-platform'), $order); ?></h1>
+        <?php else: ?>
 
-        <p class="woocommerce-notice woocommerce-notice--success woocommerce-thankyou-order-received"><?php echo apply_filters('woocommerce_thankyou_order_received_text', esc_html__('Thank you. Your donation has been received.', 'wc-donation-platform'), $order); // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?></p>
+            <h1 class="wcdp-heading wcdp-donation-received-heading">
+                <?php echo apply_filters('wcdp_thankyou_donation_received_heading', esc_html__('Thank you for your donation', 'wc-donation-platform'), $order); ?>
+            </h1>
 
-        <ul class="woocommerce-order-overview woocommerce-thankyou-order-details order_details">
+            <p class="woocommerce-notice woocommerce-notice--success woocommerce-thankyou-order-received">
+                <?php echo apply_filters('woocommerce_thankyou_order_received_text', esc_html__('Thank you. Your donation has been received.', 'wc-donation-platform'), $order); // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?>
+            </p>
 
-            <li class="woocommerce-order-overview__order order">
-                <?php esc_html_e('Donation number:', 'wc-donation-platform'); ?>
-                <strong><?php echo esc_html($order->get_order_number()); ?></strong>
-            </li>
+            <ul class="woocommerce-order-overview woocommerce-thankyou-order-details order_details">
 
-            <li class="woocommerce-order-overview__date date">
-                <?php esc_html_e('Date:', 'woocommerce'); ?>
-                <strong><?php echo esc_html(wc_format_datetime($order->get_date_created())); ?></strong>
-            </li>
-
-            <?php if (is_user_logged_in() && $order->get_user_id() === get_current_user_id() && $order->get_billing_email()) : ?>
-                <li class="woocommerce-order-overview__email email">
-                    <?php esc_html_e('Email:', 'woocommerce'); ?>
-                    <strong><?php echo sanitize_email($order->get_billing_email()); ?></strong>
+                <li class="woocommerce-order-overview__order order">
+                    <?php esc_html_e('Donation number:', 'wc-donation-platform'); ?>
+                    <strong><?php echo $order->get_order_number(); // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?></strong>
                 </li>
-            <?php endif; ?>
 
-            <li class="woocommerce-order-overview__total total">
-                <?php esc_html_e('Total:', 'woocommerce'); ?>
-                <strong><?php echo $order->get_formatted_order_total(); // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?></strong>
-            </li>
-
-            <?php if ($order->get_payment_method_title()) : ?>
-                <li class="woocommerce-order-overview__payment-method method">
-                    <?php esc_html_e('Payment method:', 'woocommerce'); ?>
-                    <strong><?php echo wp_kses_post($order->get_payment_method_title()); ?></strong>
+                <li class="woocommerce-order-overview__date date">
+                    <?php esc_html_e('Date:', 'woocommerce'); ?>
+                    <strong><?php echo wc_format_datetime($order->get_date_created()); // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?></strong>
                 </li>
-            <?php endif; ?>
 
-        </ul>
+                <?php if (is_user_logged_in() && $order->get_user_id() === get_current_user_id() && $order->get_billing_email()): ?>
+                    <li class="woocommerce-order-overview__email email">
+                        <?php esc_html_e('Email:', 'woocommerce'); ?>
+                        <strong><?php echo $order->get_billing_email(); // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?></strong>
+                    </li>
+                <?php endif; ?>
 
-    <?php endif; ?>
+                <li class="woocommerce-order-overview__total total">
+                    <?php esc_html_e('Total:', 'woocommerce'); ?>
+                    <strong><?php echo $order->get_formatted_order_total(); // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?></strong>
+                </li>
+
+                <?php if ($order->get_payment_method_title()): ?>
+                    <li class="woocommerce-order-overview__payment-method method">
+                        <?php esc_html_e('Payment method:', 'woocommerce'); ?>
+                        <strong><?php echo wp_kses_post($order->get_payment_method_title()); ?></strong>
+                    </li>
+                <?php endif; ?>
+
+            </ul>
+
+        <?php endif; ?>
 
         <?php do_action('woocommerce_thankyou_' . $order->get_payment_method(), $order->get_id()); ?>
         <?php do_action('woocommerce_thankyou', $order->get_id()); ?>
 
-    <?php else : ?>
+    <?php else: ?>
 
         <ul class="woocommerce-error" role="alert" id="wcdp-invalid-order">
             <?php esc_html_e('Your request could not be processed correctly. If the problem persists, please contact us.', 'wc-donation-platform'); ?>
         </ul>
-
     <?php endif; ?>
 
 </div>
