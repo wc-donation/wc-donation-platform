b5d26834a2f53c883b65148babd82b70c35bd134
jonas-hoebenreich <jonas.hoebenreich@tum.de>
2025-03-11 16:53:10 +0100

    set version to 1.3.4 Update wc-donation-platform.pot, package-lock.json, and wc-donation-platform.php

--- wc-donation-platform.php
+++ wc-donation-platform.php
@@ -4,7 +4,7 @@
  * Plugin URI: https://www.wc-donation.com/
  * Description: Donation Platform for WooCommerce unlocks the power of WooCommerce for your online fundraising & crowdfunding.
  * Author: Jonas Höbenreich
- * Version: 1.3.3
+ * Version: 1.3.4
  * Author URI: https://www.jonh.eu/
  * Plugin URI:  https://www.wc-donation.com/
  * License: GNU General Public License v2.0
