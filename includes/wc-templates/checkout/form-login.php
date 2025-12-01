bc1da4ac85a0b324c0c183a02c09e2b9a3d2c30e
jonas-hoebenreich <jonas.hoebenreich@tum.de>
2025-12-01 21:49:27 +0100

    chore: update WC templates to latest version

--- includes/wc-templates/checkout/form-login.php
+++ includes/wc-templates/checkout/form-login.php
@@ -14,25 +14,41 @@
  *
  * @see https://woocommerce.com/document/template-structure/
  * @package WooCommerce\Templates
- * @version 3.8.0
+ * @version 10.0.0
  */
 
 defined('ABSPATH') || exit;
 
-if (is_user_logged_in() || 'no' === get_option('woocommerce_enable_checkout_login_reminder')) {
+$registration_at_checkout = WC_Checkout::instance()->is_registration_enabled();
+$login_reminder_at_checkout = 'yes' === get_option('woocommerce_enable_checkout_login_reminder');
+
+if (is_user_logged_in()) {
     return;
 }
 
-?>
-<div class="woocommerce-form-login-toggle">
-    <?php wc_print_notice(apply_filters('woocommerce_checkout_login_message', esc_html__('Returning donor?', 'wc-donation-platform')) . ' <a href="#" class="showlogin">' . esc_html__('Click here to login', 'woocommerce') . '</a>', 'notice'); ?>
-</div>
-<?php
+if ($login_reminder_at_checkout): ?>
+    <div class="woocommerce-form-login-toggle">
+        <?php
+        wc_print_notice(
+            apply_filters('woocommerce_checkout_login_message', esc_html__('Returning donor?', 'wc-donation-platform')) . // phpcs:ignore WooCommerce.Commenting.CommentHooks.MissingHookComment
+            ' <a href="#" class="showlogin">' . esc_html__('Click here to login', 'woocommerce') . '</a>',
+            'notice'
+        );
+        ?>
+    </div>
+    <?php
+endif;
 
-woocommerce_login_form(
-    array(
-        'message' => esc_html__('Sign in with your account to contribute faster!', 'wc-donation-platform'),
-        'redirect' => wc_get_checkout_url(),
-        'hidden' => true,
-    )
-);
+if ($registration_at_checkout || $login_reminder_at_checkout):
+
+    // Always show the form after a login attempt.
+    $show_form = isset($_POST['login']); // phpcs:ignore WordPress.Security.NonceVerification.Missing
+
+    woocommerce_login_form(
+        array(
+            'message' => esc_html__('Sign in with your account to contribute faster!', 'wc-donation-platform'),
+            'redirect' => wc_get_checkout_url(),
+            'hidden' => !$show_form,
+        )
+    );
+endif;
--- includes/wc-templates/emails/admin-cancelled-order.php
+++ includes/wc-templates/emails/admin-cancelled-order.php
@@ -9,23 +9,30 @@
  * maintain compatibility. We try to do this as little as possible, but it does
  * happen. When this occurs the version of the template file will be bumped and
  * the readme will list any important changes.
+ * 
+ * forked from WooCommerce\Templates
  *
- * @see https://docs.woocommerce.com/document/template-structure/
+ * @see https://woocommerce.com/document/template-structure/
  * @package WooCommerce\Templates\Emails
- * @version 4.1.0
+ * @version 9.8.0
  */
+
+use Automattic\WooCommerce\Utilities\FeaturesUtil;
 
 if (!defined('ABSPATH')) {
     exit;
 }
 
+$email_improvements_enabled = FeaturesUtil::feature_is_enabled('email_improvements');
+
 /*
  * @hooked WC_Emails::email_header() Output the email header
-*/
+ */
 do_action('woocommerce_email_header', $email_heading, $email); ?>
 
 <?php /* translators: %1$s: Order number, %2$s: Customer full name  */ ?>
-    <p><?php printf(esc_html__('Notification to let you know &mdash; donation #%1$s belonging to %2$s has been cancelled:', 'wc-donation-platform'), esc_html($order->get_order_number()), esc_html($order->get_formatted_billing_full_name())); ?></p>
+<p><?php printf(esc_html__('Notification to let you know &mdash; donation #%1$s belonging to %2$s has been cancelled:', 'wc-donation-platform'), esc_html($order->get_order_number()), esc_html($order->get_formatted_billing_full_name())); ?>
+</p>
 
 <?php
 /*
@@ -51,7 +58,9 @@ do_action('woocommerce_email_customer_details', $order, $sent_to_admin, $plain_t
  * Show user-defined additional content - this is set in each email's settings.
  */
 if ($additional_content) {
+    echo $email_improvements_enabled ? '<table border="0" cellpadding="0" cellspacing="0" width="100%"><tr><td class="email-additional-content">' : '';
     echo wp_kses_post(wpautop(wptexturize($additional_content)));
+    echo $email_improvements_enabled ? '</td></tr></table>' : '';
 }
 
 /*
